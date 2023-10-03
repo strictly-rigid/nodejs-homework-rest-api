1b98455de2c8f4bec80a3a2b3a3d33e09129506f
strictly-rigid <holysociology@gmail.com>
2023-10-03 15:52:23 +0300

    authenticate middleware edited

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,13 +9,13 @@ const { SECRET_KEY } = process.env;
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (!token || bearer !== "Bearer") {
     next(HttpError(401));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    if (!user || !user.token || user.token !== token) {
+    if (!user) {
       next(HttpError(401));
     }
     req.user = user;

0c3a0e182f3d13e24c176209bdf609d4b825bb67
strictly-rigid <holysociology@gmail.com>
2023-10-09 14:45:13 +0300

    change send mail

--- controllers/authentication/registerUser.js
+++ controllers/authentication/registerUser.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 const { META_PASSWORD } = process.env;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const nodemailerConfig = {
   host: "smtp.meta.ua",
   port: 465,
@@ -18,8 +19,6 @@ const transport = nodemailer.createTransport(nodemailerConfig);
 const { User } = require("../../models/user");
 const { HttpError } = require("../../helpers");
 const { nanoid } = require("nanoid");
-
-// const { transport } = require("../../helpers");
 
 const { BASE_URL } = process.env;
 
@@ -44,7 +43,7 @@ const register = async (req, res) => {
 
   const verifyEmail = {
     to: email,
-    // from: "henri33@ethereal.email",
+    from: "emailer-project@meta.ua",
     subject: "Verify email",
     html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click verify email</a>`,
   };
